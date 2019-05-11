ce71510e14bffc4674ed1e075c84f452bd5967d0
sw-yx <swyx@dontemail.me>
2019-05-11 03:37:43 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,207 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { createMuiTheme, withStyles } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Hidden from "@material-ui/core/Hidden"
+import Navigator from "./Navigator"
+import Content from "./Content"
+import Header from "./Header"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let theme = createMuiTheme({
+  typography: {
+    h5: {
+      fontWeight: 500,
+      fontSize: 26,
+      letterSpacing: 0.5
+    }
+  },
+  palette: {
+    primary: {
+      light: "#63ccff",
+      main: "#009be5",
+      dark: "#006db3"
+    }
+  },
+  shape: {
+    borderRadius: 8
+  }
+})
+
+theme = {
+  ...theme,
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        backgroundColor: "#18202c"
+      }
+    },
+    MuiButton: {
+      label: {
+        textTransform: "none"
+      },
+      contained: {
+        boxShadow: "none",
+        "&:active": {
+          boxShadow: "none"
+        }
+      }
+    },
+    MuiTabs: {
+      root: {
+        marginLeft: theme.spacing(1)
+      },
+      indicator: {
+        height: 3,
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
+        backgroundColor: theme.palette.common.white
+      }
+    },
+    MuiTab: {
+      root: {
+        textTransform: "none",
+        margin: "0 16px",
+        minWidth: 0,
+        padding: 0,
+        [theme.breakpoints.up("md")]: {
+          padding: 0,
+          minWidth: 0
+        }
+      }
+    },
+    MuiIconButton: {
+      root: {
+        padding: theme.spacing(1)
+      }
+    },
+    MuiTooltip: {
+      tooltip: {
+        borderRadius: 4
+      }
+    },
+    MuiDivider: {
+      root: {
+        backgroundColor: "#404854"
+      }
+    },
+    MuiListItemText: {
+      primary: {
+        fontWeight: theme.typography.fontWeightMedium
+      }
+    },
+    MuiListItemIcon: {
+      root: {
+        color: "inherit",
+        marginRight: 0,
+        "& svg": {
+          fontSize: 20
+        }
+      }
+    },
+    MuiAvatar: {
+      root: {
+        width: 32,
+        height: 32
+      }
+    }
+  },
+  props: {
+    MuiTab: {
+      disableRipple: true
+    }
+  },
+  mixins: {
+    ...theme.mixins,
+    toolbar: {
+      minHeight: 48
+    }
+  }
 }
 
-export default App;
+const drawerWidth = 256
+
+const styles = {
+  root: {
+    display: "flex",
+    minHeight: "100vh"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appContent: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column" as flexDirection
+  },
+  mainContent: {
+    flex: 1,
+    padding: "48px 36px 0",
+    background: "#eaeff1"
+  }
+}
+
+type flexDirection =
+  | "-moz-initial"
+  | "inherit"
+  | "initial"
+  | "revert"
+  | "unset"
+  | "column"
+  | "column-reverse"
+  | "row"
+  | "row-reverse"
+  | undefined
+type Props = {
+  classes: {
+    root: string
+    drawer: string
+    appContent: string
+    mainContent: string
+  }
+}
+type State = { mobileOpen: boolean }
+class Paperbase extends React.Component<Props, State> {
+  state = {
+    mobileOpen: false
+  }
+
+  handleDrawerToggle = () => {
+    this.setState((state) => ({ mobileOpen: !state.mobileOpen }))
+  }
+
+  render() {
+    const { classes } = this.props
+
+    return (
+      <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <CssBaseline />
+          <nav className={classes.drawer}>
+            <Hidden smUp implementation="js">
+              <Navigator
+                PaperProps={{ style: { width: drawerWidth } }}
+                variant="temporary"
+                open={this.state.mobileOpen}
+                onClose={this.handleDrawerToggle}
+              />
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <Navigator PaperProps={{ style: { width: drawerWidth } }} />
+            </Hidden>
+          </nav>
+          <div className={classes.appContent}>
+            <Header onDrawerToggle={this.handleDrawerToggle} />
+            <main className={classes.mainContent}>
+              <Content />
+            </main>
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
+}
+
+export default withStyles(styles)(Paperbase)
